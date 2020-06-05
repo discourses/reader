9c40b3a723cb2766448fd493f230fc402fddd858
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-06-05 11:02:46 +0100

    Add src/io package
    Adds python modules dealing with
    
    * The package's input arguments: arguments.py
    * Directories preparation: directories.py
    * Sources from whence data will be downloaded: souce.py

---
+++ src/io/directories.py
@@ -0,0 +1,53 @@
+import os
+
+
+class Directories:
+
+    def __init__(self, var):
+        """
+        The constructor
+
+        :param var: A dot map of parameters
+        """
+
+        self.var = var
+
+    def cleanup(self):
+        """
+        Clears the directories for archived & de-archived data
+
+        :return: None
+        """
+
+        # Foremost, delete files
+        for path in [self.var.archives, self.var.data]:
+            files_ = [os.remove(os.path.join(base, file))
+                      for base, _, files in os.walk(path) for file in files]
+
+            if any(files_):
+                raise Exception('Unable to delete all files within path {}'.format(path))
+
+        # ... then, directories
+        for path in [self.var.archives, self.var.data]:
+            directories_ = [os.removedirs(os.path.join(base, directory))
+             for base, directories, _ in os.walk(path, topdown=False) for directory in directories
+             if os.path.exists(os.path.join(base, directory))]
+
+            if any(directories_):
+                raise Exception('Unable to delete all directories within path {}'.format(path))
+
+    def paths(self):
+        """
+        Creates directories for (a) archived files, (b) de-archived files
+
+        :return: None
+        """
+
+        for path in [self.var.archives, self.var.data]:
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+    def exc(self):
+
+        self.cleanup()
+        self.paths()
