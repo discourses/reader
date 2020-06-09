457506b69b4d73944e1c1a6e747124bf6e7173fb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-06-09 15:28:55 +0100

    Update src/read/interface.py: in progress

--- src/read/interface.py
+++ src/read/interface.py
@@ -19,6 +19,9 @@ class Interface:
         self.dearchive = src.read.dearchive.Dearchive(self.var)
         self.retrieve = src.read.retrieve.Retrieve(self.var)
 
+        # Parallel processing anchor
+        # self.pool = mp.Pool(mp.cpu_count())
+
     @staticmethod
     def iterables(metadata: pandas.DataFrame) -> (list, list):
         """
@@ -51,9 +54,13 @@ class Interface:
         # This set-up makes it easy to consider other types of archived files over time
         if self.var.source.archived:
             {
+                # '.zip': self.pool.starmap(self.dearchive.unzip, [{i} for i in urlstrings])
                 '.zip': mp.pool.ThreadPool().imap_unordered(self.dearchive.unzip, urlstrings)
             }.get(self.var.source.ext, LookupError('Unknown extension'))
+            mp.pool.ThreadPool().close()
         else:
+            # self.pool.starmap(self.retrieve.exc, zip(urlstrings, filestrings))
             mp.pool.ThreadPool().imap_unordered(self.retrieve.exc, zip(urlstrings, filestrings))
+            mp.pool.ThreadPool().close()
 
         print('Download finished')
