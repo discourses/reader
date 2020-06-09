1f8b45e4b3525fc0ef1f583d74f27fa21a691f6c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-06-09 13:40:50 +0100

    Update src/read/interface.py: Pool & ThreadPool

--- src/read/interface.py
+++ src/read/interface.py
@@ -19,9 +19,6 @@ class Interface:
         self.dearchive = src.read.dearchive.Dearchive(self.var)
         self.retrieve = src.read.retrieve.Retrieve(self.var)
 
-        # Parallel processing anchor
-        self.pool = mp.Pool(mp.cpu_count())
-
     @staticmethod
     def iterables(metadata: pandas.DataFrame) -> (list, list):
         """
@@ -38,6 +35,9 @@ class Interface:
 
     def exc(self, metadata: pandas.DataFrame) -> None:
         """
+        Slow:
+            '.zip': self.pool.starmap(self.dearchive.unzip, [{i} for i in urlstrings])
+            self.pool.starmap(self.retrieve.exc, zip(urlstrings, filestrings))
 
         :param metadata: A dataframe that includes a field of url strings
                          and file name strings; each must include a file extension.
@@ -46,14 +46,14 @@ class Interface:
 
         urlstrings, filestrings = self.iterables(metadata=metadata)
 
-        print('Download step starting ...')
+        print('Download starting ...')
 
         # This set-up makes it easy to consider other types of archived files over time
         if self.var.source.archived:
             {
-                '.zip': self.pool.starmap(self.dearchive.unzip, [{i} for i in urlstrings])
+                '.zip': mp.pool.ThreadPool().imap_unordered(self.dearchive.unzip, urlstrings)
             }.get(self.var.source.ext, LookupError('Unknown extension'))
         else:
-            self.pool.starmap(self.retrieve.exc, zip(urlstrings, filestrings))
+            mp.pool.ThreadPool().imap_unordered(self.retrieve.exc, zip(urlstrings, filestrings))
 
-        print('Download step ended')
+        print('Download finished')
